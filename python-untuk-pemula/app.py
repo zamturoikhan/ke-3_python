682832c4b200afbea37c33747ed44c049eddcb82
unknown <mzamturoikhan@gmail.com>
2020-02-15 21:44:17 +0700

    KE-2

--- python-untuk-pemula/app.py
+++ python-untuk-pemula/app.py
@@ -1,1 +1,28 @@
-print("hello world dumbways.id")
+# string, integer, duoble, float. boolean, array, obj
+# string
+x = "x"
+food = "apple pie"
+my_favorite_food = "cheese burger"
+myFavoriteFood = "kebab"
+print(x)
+print(food)
+print(my_favorite_food)
+print(myFavoriteFood) 
+
+# integer
+number = 1
+first_number = 2
+second_number = 3
+
+# print(second_number + first_number)
+
+# double dan float
+decimal = 1.2
+my_decimal = 3.3
+
+print(decimal + my_decimal)
+
+# boolean
+is_clean = True
+
+print(is_clean and False)
